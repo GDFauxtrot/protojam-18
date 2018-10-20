35320502a3d435f143564367d4584492f88c5cb7
Luke M <lukeam@uci.edu>
2018-10-19 20:37:34 -0700

    Added a skeleton for Explodeable

--- Assets/Scripts/Explodeable.cs
+++ Assets/Scripts/Explodeable.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Explodeable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    //Tells the GameManager to increase the multiplier by .1
+    double multiplierUp()
+    {
+
+        return .1;
+    }
+    
+    //tells the GameManneger to increase the multiple by mul
+    double multiplierUp(double mul)
+    {
+
+        return mul;
+    }
+
+    //Tells the GameManager to give the player a set number of points equal to 100
+    int givePoints()
+    {
+
+        return 100;
+    }
+
+    //Tells the GameManager to give the player a given number of points equal to pnt
+    int givePoints(int pnt)
+    {
+        
+
+        return pnt;
+    }
+
+    //Removes the explodable from the map
+    void disapear()
+    {
+
+    }
+
+    //Plays a animation of the explosion
+    void explode()
+    {
+
+    }
+
+    //Tells the GameManager that this Object has Exploded
+	void tellGM()
+    {
+
+    }
 }
